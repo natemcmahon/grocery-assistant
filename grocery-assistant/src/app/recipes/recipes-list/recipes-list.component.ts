019b7592688ab0a577ce4c7c588c075e7451f9e4
Nathaniel McMahon <nathaniel.mcmahon@hrblock.com>
2024-10-21 16:32:08 -0500

    wireframe setup

--- grocery-assistant/src/app/recipes/recipes-list/recipes-list.component.ts
+++ grocery-assistant/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './recipes-list.component.css'
 })
 export class RecipesListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'A simple test', 'https://www.allrecipes.com/thmb/oWrKte1auLsqVEcnxa8z1vCZdDU=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/67700_RichPastaforthePoorKitchen_ddmfs_4x3_2284-220302ec8328442096df370dede357d7.jpg')
+  ];
 }
